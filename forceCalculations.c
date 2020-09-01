f0062e6e9edbcc69acc26f444c4b3853792bfdfd
lrlong <66316581+lrlong@users.noreply.github.com>
2020-09-01 14:46:51 -0400

    Update forceCalculations.c

--- forceCalculations.c
+++ forceCalculations.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 extern int targetTenacity;
+extern int targetDefense;
 
 int force_lightning(int power) {
 	return targetTenacity - (power * 2);
@@ -12,4 +13,8 @@ int force_choke(int strength) {
 
 int force_push(int power) {
 	return targetTenacity - power;
+}
+
+int force_saber_throw(int accuracy, int strength){
+	return targetDefense - (strength * accuracy);
 }
