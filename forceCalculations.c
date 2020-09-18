8ecc169ef3d69410a5702b1f8cca23b9ebce7966
lrlong <66316581+lrlong@users.noreply.github.com>
2020-09-18 10:34:22 -0400

    Update forceCalculations.c

--- forceCalculations.c
+++ forceCalculations.c
@@ -5,18 +5,15 @@ extern int targetDefense;
 extern int targetHealth;
 
 int force_test(int test) {
-	// Test
 	return test + 1;
 }
 
 int force_drain(int power) {
-	// Test
 	int x = power * (targetTenacity * 0.01);
 	return targetHealth * x;
 }
 
 int force_lightning(int power) {
-	// Test
 	return targetTenacity - (power * 2);
 }
 
@@ -33,8 +30,6 @@ int force_saber_throw(int accuracy, int strength){
 }
 
 int force_heal(int lifeForce, int power, int strength) {
-	// This is a test
-	// Some comment here for fun.
 	int y = power * strength;
 	int x = power * (lifeForce * 0.1);
 	int z = x + y;
